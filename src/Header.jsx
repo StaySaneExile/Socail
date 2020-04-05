6fc27d1af6589bcafe901bc4702765355180b28a
StaySaneExile <vasiliy2490@gmail.com>
2020-04-05 15:11:49 +0500

    first shit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+            < header
+                className = "header" >
+                < img src = "https://png.pngtree.com/template/20190809/ourlarge/pngtree-golden-building-real-estate-logo-icon-concept-template-on-dark-background-image_290695.jpg" />
+            </header>
+    );
+}
+
+
+export default Header;
---
+++ src/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        < div className="content">
+        < div>
+         < img
+           src="https://www.exler.ru/films/images/10-03-2011/big/screen00002.jpg" / >
+         < /div>
+      < div> ava + description < /div>
+        <div> mypost
+         < div> newpost < /div>
+          < div>
+             < div> post1 < /div>
+           < /div>
+              < /div>
+               < /div>
+
+);
+ }
+
+
+  export default Profile;
