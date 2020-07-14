f15333b8eef9c57916dac5471d60069cd4f83327
StaySaneExile <vasiliy2490@gmail.com>
2020-07-14 18:24:08 +0500

    Updates

---
+++ precache-manifest.e47be8a00e14180259c62871e1c82d48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db17f8ae35d9d824a7fd986e79c07753",
+    "url": "/Socail/index.html"
+  },
+  {
+    "revision": "0d20f06d251501414fab",
+    "url": "/Socail/static/css/main.29a3b4ec.chunk.css"
+  },
+  {
+    "revision": "7ac49df2484908b6c020",
+    "url": "/Socail/static/js/2.9ebc4ae9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Socail/static/js/2.9ebc4ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d20f06d251501414fab",
+    "url": "/Socail/static/js/main.cc4a0b64.chunk.js"
+  },
+  {
+    "revision": "e300e08d796797cdb284",
+    "url": "/Socail/static/js/runtime-main.9ec791ce.js"
+  },
+  {
+    "revision": "a4450bda4388a66ace0ba7e4b868ba14",
+    "url": "/Socail/static/media/preloader.a4450bda.gif"
+  },
+  {
+    "revision": "d38be7c12ce8606776d79eec37e1f709",
+    "url": "/Socail/static/media/user.d38be7c1.png"
+  }
+]);
